9c83f570075ced5742d2b140b86b4d2d7cba561b
carlcarl <carlcarlking@gmail.com>
2015-08-04 16:24:26 +0800

    Use function to get version

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from setuptools import setup
 from setuptools import find_packages
 
+import re
+import os
+
+def get_release():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    root = os.path.dirname(__file__)
+    init_py = os.path.join(root, 'dcard_coworker', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            raise RuntimeError('Cannot find version in dcard_coworker/__init__.py')
+
+
 setup(
     name='dcard_coworker',
     description='',
     long_description=open('README.rst').read(),
-    version='0.0.1',
+    version=get_release(),
     author='carlcarl',
     author_email='carlcarlking@gmail.com',
     url='https://github.com/carlcarl/dcard_coworker',
